335cc752202b815cb612c230d2344b35b020e557
Shrashti Kesharwani <53169813+ShrashtiKesharwani@users.noreply.github.com>
2021-06-22 20:47:13 +0530

    Delete RPM-Product-Microservice directory

--- RPM-Product-Microservice/bin/src/main/java/com/cognizant/microservice/exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.microservice.exception;
-
-public class ProductNotFoundException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public ProductNotFoundException(String msg) {
-		super(msg);
-	}
-}
