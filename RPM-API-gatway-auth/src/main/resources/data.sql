760797db14da95123c54ee066b834ba35a7831af
Shrashti Kesharwani <53169813+ShrashtiKesharwani@users.noreply.github.com>
2021-06-22 18:06:08 +0530

    Add files via upload

---
+++ RPM-API-gatway-auth/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values('shrashti','shrashti',1);
+insert into user values('vaibhav','vaibhav',2);
+insert into user values('nikhil','nikhil',9);
+insert into user values('sharan','sharan',3);
+insert into user values('gowtham','gowtham',5);
